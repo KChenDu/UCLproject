7fb510b9bd52daf6d9491638fed183d6b5e60152
Orange <47792375+KChenDu@users.noreply.github.com>
2024-04-05 02:56:52 +0100

    update generate method

--- generate.py
+++ generate.py
@@ -1,28 +1,32 @@
 import argparse
 
-from pathlib import Path
-from loguru import logger
-from json import loads, dumps
+from human_eval.data import read_problems, write_jsonl
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from tqdm import tqdm
+
+
+def generate_one_completion(prompt: str) -> str:
+    inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+    outputs = model.generate(**inputs, max_length=256, pad_token_id=tokenizer.eos_token_id)
+    completion = tokenizer.decode(outputs[0], skip_special_tokens=True)[len(prompt):]
+    return completion
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('file')
+    parser.add_argument('num_samples_per_task', type=int)
     args = parser.parse_args()
 
-    path = Path(args.file)
+    num_samples_per_task = args.num_samples_per_task
 
+    problems = read_problems()
+    length = len(problems)
+    samples = [None] * length * num_samples_per_task
     tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base", trust_remote_code=True)
     model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base", trust_remote_code=True).cuda()
 
-    with open(path, 'r') as f, open(path.stem + '_generation.jsonl', 'w') as g:
-        for i, line in enumerate(f):
-            logger.info(f"Processing line {i + 1}...")
-            datum = loads(line)
-            task_id = datum["task_id"]
-            input_text = loads(line)["prompt"]
-            inputs = tokenizer(input_text, return_tensors="pt").to(model.device)
-            outputs = model.generate(**inputs, max_length=512)
-            g.write(dumps({"task_id": task_id, "generation": tokenizer.decode(outputs[0], skip_special_tokens=True)}) + '\n')
-            logger.info(f"Line {i + 1} processed.")
+    for i in range(num_samples_per_task):
+        for j, task_id in enumerate(tqdm(problems, f"sample {i + 1}", leave=False, unit="problem")):
+            samples[i * length + j] = dict(task_id=task_id, completion=generate_one_completion(problems[task_id]["prompt"]))
+
+    write_jsonl("samples.jsonl", samples)
