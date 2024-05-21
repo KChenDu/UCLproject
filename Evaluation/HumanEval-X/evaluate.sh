c0df13237ace1426ffeb7217e3b54c3811d79190
Orange <47792375+KChenDu@users.noreply.github.com>
2024-05-21 16:48:33 +0100

    small fix

--- Evaluation/HumanEval-X/evaluate.sh
+++ Evaluation/HumanEval-X/evaluate.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-python -m evaluate.py --model "${1:-deepseek-ai/deepseek-coder-1.3b-base}" --language "${2:-python}" --num_samples_per_task "${3:-1}"
+#!/bin/zsh
+python generate.py --model "${1:-deepseek-ai/deepseek-coder-1.3b-base}" --language "${2:-python}" --num_samples_per_task "${3:-1}"
--- Evaluation/HumanEval/evaluate.sh
+++ Evaluation/HumanEval/evaluate.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-python -m evaluate.py --model "${1:-deepseek-ai/deepseek-coder-1.3b-base}" --num_samples_per_task "${2:-1}"
+#!/bin/zsh
+python generate.py --model "${1:-deepseek-ai/deepseek-coder-1.3b-base}" --num_samples_per_task "${2:-1}"
 evaluate_functional_correctness humaneval_samples.jsonl
