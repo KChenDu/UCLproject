dd12fcf4204dd85558b22bc3a1edd248a00a6ff7
Orange <kelvenchendu@gmail.com>
2024-07-03 13:29:48 +0800

    upload training script

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from json import loads
+from datasets import Dataset
 
 
 dpo_dataset_dict = {
@@ -14,31 +15,29 @@ with open('MBPP(python)_nucleus92_demo.jsonl', 'r') as file:
         dpo_dataset_dict["chosen"].append(datum["code"])
         dpo_dataset_dict["rejected"].append(datum["generation"])
 
-from transformers import AutoModelForCausalLM
-from peft import LoraConfig, get_peft_model
-
-
-base_model = AutoModelForCausalLM.from_pretrained('deepseek-ai/deepseek-coder-1.3b-base', trust_remote_code=True).cuda()
-
-lora_config = LoraConfig(r=1)
-peft_model = get_peft_model(base_model, lora_config)
-
-for e in base_model.modules():
-    print(e)
-
-from trl import DPOConfig, DPOTrainer
-from transformers import AutoTokenizer
-from datasets import Dataset
-
-
-training_args = DPOConfig(beta=0.1, output_dir='temp')
-tokenizer = AutoTokenizer.from_pretrained('deepseek-ai/deepseek-coder-1.3b-base', trust_remote_code=True)
-
 train_dataset = Dataset.from_dict(dpo_dataset_dict)
 
+
+from trl import DPOConfig, DPOTrainer
+from peft import LoraConfig, LoraModel
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+training_args = DPOConfig(beta=0.1,
+                          output_dir='checkpoints',
+                          max_length=1024,
+                          max_prompt_length=2048,
+                          remove_unused_columns=False,
+                          per_device_train_batch_size=1,
+                          gradient_accumulation_steps=4
+)
+config = LoraConfig(task_type="CAUSAL_LM", r=1)
+tokenizer = AutoTokenizer.from_pretrained('deepseek-ai/deepseek-coder-1.3b-base', trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained('deepseek-ai/deepseek-coder-1.3b-base', trust_remote_code=True).cuda()
+lora_model = LoraModel(model, config, "default")
 dpo_trainer = DPOTrainer(
-    peft_model,
-    None,
+    lora_model,
+    model,
     args=training_args,
     train_dataset=train_dataset,
     tokenizer=tokenizer,  # for visual language models, use tokenizer=processor instead
